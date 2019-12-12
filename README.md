8437c8a8a11e6a4b79c49c6868747bf5d88d481f
magda <stenius.mimmy@gmail.com>
2019-12-12 19:24:41 +0200

    Start project and add setup instructions.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Robotics Guild Codebase
+
+## Getting started
+1. Create a virtual environment: `python3 -m virtualenv venv`
+2. Activate it: `source venv/bin/activate`
+3. Install requirements: `pip3 install -r requirements.txt`
+4. If you don't already have it, install the EV3DEV device browser plugin to your VS Code.
+5. Connect the ev3dev with your VS Code via USB or WiFi.
+
+## Ports
+Outputs = Port.A, Port.B.... etc.  
+Inputs = Port.S1, Port.S2... etc.
+
---
+++ main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C
+from ev3dev2.sensor import INPUT_3, INPUT_2
+
+from roina import Claw, Movement, Controller, LineFollower, BoxZone, BM, PressButton
+import sys
+import logging
+from threading import Thread
+
+# Outputs = Port.A, Port.B.... etc
+# Inputs = Port.S1, Port.S2... etc
+# DOcs https://le-www-live-s.legocdn.com/sc/media/files/ev3-micropython/ev3micropythonv100-71d3f28c59a1e766e92a59ff8500818e.pdf
+
+
+logging.getLogger().setLevel(logging.INFO)
+
+class Main(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        theEnd = False
+        bm = BM()
+        bm.beep()
+        while not theEnd:
+            move = Movement(OUTPUT_A, OUTPUT_B, INPUT_3, INPUT_2)
+
+            # first section MAZE
+            follower = LineFollower(INPUT_2, INPUT_3, OUTPUT_A, OUTPUT_B, OUTPUT_C)
+            follower.followLine()
+
+
+
+Main()
+
+while True:
+    pass
